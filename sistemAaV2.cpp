c0dc54a2a484036c5be86d24f2ea50754b7cf99b
layerZ-c <50850709+layerZ-c@users.noreply.github.com>
2019-08-25 15:11:02 -0300

    Inseridos novos relatorios
    Relatório de produtos por valor -> Exibe os produtos maior que, menor que e entre um intervalo de valor informado pelo usuário.
    Relatório de produtos cadastrados no sistema -> Exibe todos os produtos cadastrados.

--- sistemAaV2.cpp
+++ sistemAaV2.cpp
@@ -3,8 +3,8 @@
 #include <string.h>
 
 /*
-*	crie um arquivo produtos.txt antes de compilar
-*	abçs
+*	pode compilar direto, sem criar o arquivo produtos.txt na pasta
+*	so pegar e rodar
 */
 
 FILE *ptrArquivo;
@@ -48,13 +48,17 @@ void menuUnidade_Produto();
 void lista_TipoMedida(short opInf_UndMedida);
 void lista_tipoGrupo(short opInf_GpProduto);
 void menu_cadastrarProduto();
+
+void menu_RelProdutosPorValor();
 void rel_produtoPorGrupo();
+void rel_produtosPorValor();
+void rel_produtosCadastrados();
 
 int main(){
 	cadastrarProduto();
-	
-	printf("\nPressione uma tecla para continuar...\n");
-	getchar();
+	rel_produtosPorValor();
+	rel_produtosCadastrados();
+	system("pause");
 	return 0;
 }
 
@@ -73,7 +77,7 @@ void cadastrarProduto(void){
 	scanf("%[^\n]s", cadastroProduto.nome);
 	fflush(stdin);
 
-	//transforma td em caixa alta
+	//transforma td em caixa alta colocando _ nos espacos do nome
 	short tamNome = strlen(cadastroProduto.nome);
 	for(size_t iNome = 0; iNome < tamNome; iNome++){
 		if(cadastroProduto.nome[iNome] >= 97 && cadastroProduto.nome[iNome] <= 122)
@@ -87,7 +91,7 @@ void cadastrarProduto(void){
 	if((v_EhVazio = fgetc(ptrArquivo)) == EOF) v_UltimoCod = 9999;
 	else{
 		while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
-			if(strcmp(v_Nome, cadastroProduto.nome) == 0){
+				if(strcmp(v_Nome, cadastroProduto.nome) == 0){
 				printf("ERRO 0000x2: Jah existe um produto cadastrado com esse nome!\n");
 				return;
 			}
@@ -100,19 +104,19 @@ void cadastrarProduto(void){
 	//daqui p baixo
 
 	//grupo do produto
-	system("clear");
+	system("cls");
 	short opInf_GpProduto = 0;
 	menuGrupo_Produto();
 	printf("Informe o grupo do produto: ");
 	scanf(" %d", &opInf_GpProduto);
 
 	//pontuacao do produto
-	system("clear");
+	system("cls");
 	printf("Informe a pontuacao do produto: ");
 	scanf("%f%*c", &cadastroProduto.pontuacao);
 
 	//valor do produto
-	system("clear");
+	system("cls");
 	printf("Informe o valor do produto: ");
 	scanf("%f%*c", &cadastroProduto.valor);
 
@@ -134,6 +138,12 @@ void cadastrarProduto(void){
 
 }//FIM function
 
+void menu_cadastrarProduto(){
+	printf("\n=================================================\n");
+
+	printf("\n=================================================\n");
+}
+
 void lista_tipoGrupo(short opInf_GpProduto){
 	if(opInf_GpProduto == 1) fprintf(ptrArquivo, "%s", "Embalagem ");
 	else if(opInf_GpProduto == 2) fprintf(ptrArquivo, "%s", "Bebida ");
@@ -145,22 +155,149 @@ void lista_tipoGrupo(short opInf_GpProduto){
 	else if(opInf_GpProduto == 8) fprintf(ptrArquivo, "%s", "Higienização_Pessoal ");
 	else if(opInf_GpProduto == 9) fprintf(ptrArquivo, "%s", "Semente,_Muda_de_Plantas ");
 	else if(opInf_GpProduto == 10) fprintf(ptrArquivo, "%s", "Equipamento_de_informatica ");
+	else if(opInf_GpProduto == 11) fprintf(ptrArquivo, "%s", "Material_de_Escritorio ");
+	else if(opInf_GpProduto == 12) fprintf(ptrArquivo, "%s", "Material_de_Limpeza ");
 }//FIM function
 
 void menuGrupo_Produto(){
 	printf("1-> Acondicionamento e Embalagem\n");
-	printf("2-> Alimentícios: Bebida\n");
-	printf("3-> Alimentícios: Alimento\n");
-	printf("4-> Alimentícios: Alimento animal\n");
-	printf("5-> Ferramenta de Manutenção em Geral\n");
+	printf("2-> Bebida\n");
+	printf("3-> Alimento\n");
+	printf("4-> Alimento animal\n");
+	printf("5-> Ferramenta de Manutencao em Geral\n");
 	printf("6-> Medicamento\n");
-	printf("7-> Material de Expediente: Material de Cozinha\n");
-	printf("8-> Material de Expediente: Higienização Pessoal\n");
+	printf("7-> Material de Cozinha\n");
+	printf("8-> Higienizacao Pessoal\n");
 	printf("9-> Semente, Muda de Plantas\n");
-	printf("10-> Equipamento de informatica\n");
-}
+	printf("10-> Equipamento de Informatica\n");
+	printf("11-> Material de Escritorio\n");
+	printf("12-> Material de limpeza\n");
+}//FIM function
 
 void rel_produtoPorGrupo(){
     //escreva daqui pra baixo
     
+}//FIM function 
+
+void menu_RelProdutosPorValor(){
+	system("cls");	
+	printf("========== RELATORIO DE PRODUTOS POR VALOR ==========\n\n");
+	printf("1-> Maior ou igual ao valor informado\n");
+	printf("2-> Menor ou igual ao valor informado\n");
+	printf("3-> Igual ao valor informado\n");
+	printf("4-> Entre um intervalo informado\n");
+}//FIM funtion
+
+void rel_produtosPorValor(){
+	int v_UltimoCod;
+	char v_Nome[50] = {'\0'};
+	char v_Grupo[50] = {'\0'};
+	float v_Pontuacao;
+	float v_Valor;
+	short tipoConsulta = 0;
+	float valorInf1, valorInf2;
+	menu_RelProdutosPorValor();
+	printf("Informe o tipo de consulta: ");
+	scanf(" %d", &tipoConsulta);
+	
+	if(tipoConsulta == 1){
+		printf("Informe um valor: ");
+		scanf(" %f", &valorInf1);
+		ptrArquivo = fopen("produtos.txt", "r");
+		if(ptrArquivo == NULL) exit(EXIT_FAILURE);
+		system("cls");
+		printf("========================================================\n\n");
+		while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
+			if(v_Valor >= valorInf1){
+				short tamV_Nome = strlen(v_Nome);
+				for(size_t iNome = 0; iNome < tamV_Nome; iNome++)
+				if(v_Nome[iNome] == '_') v_Nome[iNome] = ' ';
+				printf("%d - %s ---------- R$%.2f\n", v_UltimoCod, v_Nome, v_Valor);
+			} 	
+		}
+		printf("\n========================================================\n");
+	}//FIM consulta1
+	
+		if(tipoConsulta == 2){
+		printf("Informe um valor: ");
+		scanf(" %f", &valorInf1);
+		ptrArquivo = fopen("produtos.txt", "r");
+		if(ptrArquivo == NULL) exit(EXIT_FAILURE);
+		system("cls");
+		printf("========================================================\n\n");
+		while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
+			if(v_Valor <= valorInf1){
+				short tamV_Nome = strlen(v_Nome);
+				for(size_t iNome = 0; iNome < tamV_Nome; iNome++)
+				if(v_Nome[iNome] == '_') v_Nome[iNome] = ' ';
+				printf("%d - %s ---------- R$%.2f\n", v_UltimoCod, v_Nome, v_Valor);
+			} 	
+		}
+		printf("\n========================================================\n");
+	}//FIM consulta2
+	
+		if(tipoConsulta == 3){
+		printf("Informe um valor: ");
+		scanf(" %f", &valorInf1);
+		ptrArquivo = fopen("produtos.txt", "r");
+		if(ptrArquivo == NULL) exit(EXIT_FAILURE);
+		system("cls");
+		printf("========================================================\n\n");
+		while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
+			if(v_Valor == valorInf1){
+				short tamV_Nome = strlen(v_Nome);
+				for(size_t iNome = 0; iNome < tamV_Nome; iNome++)
+				if(v_Nome[iNome] == '_') v_Nome[iNome] = ' ';
+				printf("%d - %s ---------- R$%.2f\n", v_UltimoCod, v_Nome, v_Valor);
+			} 	
+		}
+		printf("\n========================================================\n");
+	}//FIM consulta3
+	
+		if(tipoConsulta == 4){
+		printf("Maior que: ");
+		scanf(" %f", &valorInf1);
+		printf("Menor que: ");
+		scanf(" %f", &valorInf2);
+		ptrArquivo = fopen("produtos.txt", "r");
+		if(ptrArquivo == NULL) exit(EXIT_FAILURE);
+		system("cls");
+		printf("========================================================\n\n");
+		while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
+			if(v_Valor >= valorInf1 && v_Valor <= valorInf2){
+				short tamV_Nome = strlen(v_Nome);
+				for(size_t iNome = 0; iNome < tamV_Nome; iNome++)
+				if(v_Nome[iNome] == '_') v_Nome[iNome] = ' ';
+				printf("%d - %s ---------- R$%.2f\n", v_UltimoCod, v_Nome, v_Valor);
+			} 	
+		}
+		printf("\n========================================================\n");
+	}//FIM consulta4
+	system("pause");
+}//FIM function
+
+void rel_produtosCadastrados(){
+	int v_UltimoCod;
+	char v_Nome[50] = {'\0'};
+	char v_Grupo[50] = {'\0'};
+	float v_Pontuacao;
+	float v_Valor;
+	ptrArquivo = fopen("produtos.txt", "r");
+	if(ptrArquivo == NULL) exit(EXIT_FAILURE);
+	system("cls");
+	printf("============ LISTAGEM DE PRODUTOS CADASTRADOS ===========\n\n");
+	printf("\nCOD   = PRODUTO ================================= VALOR\n\n");
+	while((fscanf(ptrArquivo, "%d %s %s %f %f\n", &v_UltimoCod, v_Nome, v_Grupo, &v_Pontuacao, &v_Valor)) != EOF){
+		short tamV_Nome = strlen(v_Nome);
+		short dif = 40 - tamV_Nome;
+		char v_Hifen[dif + 1] = {'\0'};
+			for(size_t iNome = 0; iNome < tamV_Nome; iNome++)
+			if(v_Nome[iNome] == '_') v_Nome[iNome] = ' ';
+			for(size_t iNome = 0; iNome < dif; iNome++)
+			v_Hifen[iNome] = 32;
+			printf("%d - %s %s R$%.2f\n", v_UltimoCod, v_Nome, v_Hifen, v_Valor);	
+		}
+		printf("\n========================================================\n");
+		fclose(ptrArquivo);
+		system("pause");
 }//FIM function
