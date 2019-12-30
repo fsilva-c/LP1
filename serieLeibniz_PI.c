cb7f3e39fe6df33cc17394c3163c4a94acb1456d
layerZ-c <50850709+layerZ-c@users.noreply.github.com>
2019-12-30 11:54:24 -0300

    Add files via upload
    Série de Leibniz

---
+++ serieLeibniz_PI.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+    serie de Leibniz:
+    4/1 - 4/3 + 4/5 - 4/7 + 4/9... = ~ pi 
+*/
+
+//definindo o intervalo de operação do laço for
+#define N 3000000
+
+int main(){
+    int sinal = -1;
+    float valor = 0., somaPi = 0.;
+
+    for(int iSoma = 1; iSoma <= N; iSoma += 2){
+        valor = 4. / iSoma;
+
+        //somando o valor da divisao de 4 pelo indiceSoma na variavel que guarda o valor de pi 
+        somaPi += valor;
+
+        //multiplicando o valor por -1 pra sequencia ficar + - + - + - + -...
+        somaPi *= sinal;
+    }
+
+    printf("O valor aproximado de PI apos %d operacoes eh = %.8f\n", N, somaPi);
+
+}//END main
